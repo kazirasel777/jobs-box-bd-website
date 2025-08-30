30e1d3a92b3ff0ba97f876652dd965ab1f800f53
kazirasel777 <67787866+kazirasel777@users.noreply.github.com>
2025-08-31 04:04:17 +0600

    Update slugify.js

--- utils/slugify.js
+++ utils/slugify.js
@@ -1,13 +1,17 @@
 // utils/slugify.js
 
-export const slugify = (text) => {
-    if (typeof text !== 'string') {
-        return '';
-    }
-    return text.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
-        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
-};
+export function generateSlug(input) {
+  if (!input) return '';
+  const text = String(input)
+    .normalize('NFKD')
+    .replace(/\u200D/g, '')      // ZWJ ইত্যাদি সরাও
+    .replace(/[^\p{L}\p{N}]+/gu, '-') // অক্ষর/সংখ্যা ছাড়া সব কিছুর জায়গায় '-'
+    .replace(/^-+|-+$/g, '');    // শুরু/শেষের '-' কেটে দাও
+  return text.toLowerCase();
+}
+
+// পুরোনো কোডে { slugify } থাকলে যাতে না ভাঙে:
+export const slugify = generateSlug;
+
+// default import সাপোর্টের জন্য:
+export default generateSlug;
